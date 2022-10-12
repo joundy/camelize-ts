15c6054c45d9720863c64a4da55ac72f55407e94
joundy <hafizjoundys@gmail.com>
2022-10-13 02:31:51 +0700

    feat: transform from camelcase to snakecase

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ type CamelCase<S extends string> =
     ? `${P1}${Uppercase<P2>}${CamelCase<P3>}`
     : S;
 
-
+type CamelToSnakeCase<S extends string> = S extends `${infer T}${infer U}`
+  ? `${T extends Capitalize<T> ? "_" : ""}${Lowercase<T>}${CamelToSnakeCase<U>}`
+  : S;
 
 export type Camelize<T> = {
   [K in keyof T as CamelCase<string & K>]: T[K] extends Array<infer U>
@@ -15,26 +17,53 @@ export type Camelize<T> = {
     : T[K];
 };
 
+export type Snakeize<T> = {
+  [K in keyof T as CamelToSnakeCase<string & K>]: T[K] extends Array<infer U>
+    ? U extends {}
+      ? Array<Snakeize<U>>
+      : T[K]
+    : T[K] extends {}
+    ? Camelize<T[K]>
+    : T[K];
+};
+
 function camelCase(str: string) {
   return str.replace(/[_.-](\w|$)/g, function (_, x) {
-      return x.toUpperCase();
+    return x.toUpperCase();
   });
 }
 
-function walk(obj): any {
-  if (!obj || typeof obj !== "object") return obj;
-  if (obj instanceof Date || obj instanceof RegExp) return obj;
-  if (Array.isArray(obj)) return obj.map(walk);
-
-  return Object.keys(obj).reduce((res, key) => {
-    const camel = camelCase(key);
-    res[camel] = walk(obj[key]);
-    return res;
-  }, {});
+function snakeCase(str: string) {
+  return str.replace(/[A-Z]/g, function (str) {
+    return `_${str.toLowerCase()}`;
+  });
 }
 
-export default function camelize<T>(
-  obj: T
-): T extends String ? string : Camelize<T> {
-  return typeof obj === "string" ? camelCase(obj) : walk(obj);
+function processWalk(obj, fn): any {
+  const walk = (obj) => {
+    if (!obj || typeof obj !== "object") return obj;
+    if (obj instanceof Date || obj instanceof RegExp) return obj;
+    if (Array.isArray(obj)) return obj.map(walk);
+
+    return Object.keys(obj).reduce((res, key) => {
+      // const camel = camelCase(key);
+      const camel = fn(key);
+      res[camel] = walk(obj[key]);
+      return res;
+    }, {});
+  };
+
+  return walk(obj);
+}
+
+export function camelize<T>(obj: T): T extends String ? string : Camelize<T> {
+  return typeof obj === "string"
+    ? camelCase(obj)
+    : processWalk(obj, (key) => camelCase(key));
+}
+
+export function snakeize<T>(obj: T): T extends String ? string : Camelize<T> {
+  return typeof obj === "string"
+    ? camelCase(obj)
+    : processWalk(obj, (key) => snakeCase(key));
 }
